2c0339311aa277d4ef2d701c73ff2a0f9b3916aa
Priyanka Jain <priyanka.jain@fulfil.io>
2015-07-27 17:35:57 +0530

    Display available quantity on shipment move line
    Description: Display available quantity on shipment move line
    
    https://basecamp.com/2993397/people/11995186

--- __init__.py
+++ __init__.py
@@ -8,11 +8,13 @@
 from trytond.pool import Pool
 from sale import SaleLine
 from product import ProductByLocationExcludeAssigned
+from stock import Move
 
 
 def register():
     Pool.register(
         SaleLine,
+        Move,
         module='sale_available_stock', type_='model'
     )
     Pool.register(
